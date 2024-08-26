2237de50e3d78e1a26feddc2eada9ed5f13df78e
swaroop0202 <swaroopk372@gmail.com>
2024-08-26 16:48:05 +0530

    M

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-echo "hi this is new shell script"
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+if [ $USERID -ne 0 ]
+    then 
+        echo "please run this script as root user"
+    else
+        echo "you are a super user"
+fi
+
+VALIDATE() {
+if [ $1 -ne 0 ]
+    then
+        echo "$2...failure"
+        exit1
+    else
+        echo "$2..success"
+fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "installing mysql server"
+
+systemctl enable mysqld
+VALIDATE $? "enabling mysql"
+
+systemctl start mysqld
+VALIDATE $? "starting mysql"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "setting the root password"
